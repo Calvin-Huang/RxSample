884d7de0f38ad90a651355238ceba544c97404df
Calvin <calvin.peak@capslock.tw>
2016-10-13 13:12:51 +0900

    Complete basic structure.

---
+++ RxSample/RepoCell.swift
@@ -0,0 +1,16 @@
+//
+//  RepoCell.swift
+//  RxSample
+//
+//  Created by Calvin on 10/13/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var browseButton: UIButton!
+    
+}
