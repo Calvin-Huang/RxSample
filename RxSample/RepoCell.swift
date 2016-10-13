6cb24be9ef327109a4b4662f0d9ff6a3a24bbafd
Calvin <calvin.peak@capslock.tw>
2016-10-13 17:42:11 +0900

    Complete repo button action.

--- RxSample/RepoCell.swift
+++ RxSample/RepoCell.swift
@@ -16,15 +16,18 @@ class RepoCell: UITableViewCell {
     var url: URL?
     
     override func awakeFromNib() {
-        browseButton.addTarget(self, action: #selector(browseButton(_:)), for: .touchDragInside)
+        browseButton.addTarget(self, action: #selector(browseButtonClicked(_:)), for: .touchUpInside)
     }
     
     weak var viewController: UIViewController?
     
-    func browseButton(_: AnyObject) {
+    func browseButtonClicked(_: AnyObject) {
+        guard let url = url else { return }
+        
         let webViewController = WebViewViewController()
         
-        
+        webViewController.loadViewIfNeeded()
+        webViewController.webView.loadRequest(URLRequest(url: url))
         viewController?.present(webViewController, animated: true, completion: nil)
     }
 }
