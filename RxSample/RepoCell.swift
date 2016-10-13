2c6e925638ef62a8cd2478db9be47ce8a1fc30f5
Calvin <calvin.peak@capslock.tw>
2016-10-13 14:18:29 +0900

    Complete table cell basic function.

--- RxSample/RepoCell.swift
+++ RxSample/RepoCell.swift
@@ -13,4 +13,18 @@ class RepoCell: UITableViewCell {
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var browseButton: UIButton!
     
+    var url: URL?
+    
+    override func awakeFromNib() {
+        browseButton.addTarget(self, action: #selector(browseButton(_:)), for: .touchDragInside)
+    }
+    
+    weak var viewController: UIViewController?
+    
+    func browseButton(_: AnyObject) {
+        let webViewController = WebViewViewController()
+        
+        
+        viewController?.present(webViewController, animated: true, completion: nil)
+    }
 }
