44f3b17e461a2b79dcf876002de84f7e9bc9e76e
Calvin <calvin.peak@capslock.tw>
2016-10-13 14:18:06 +0900

    Complete basic function.

--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -9,19 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var navigationBar: UINavigationBar!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var searchBar: UISearchBar!
 
+    var navigationBarTitle: String? {
+        set {
+            navigationBar.topItem?.title = newValue
+        }
+        
+        get {
+            return navigationBar.topItem?.title
+        }
+    }
+    
+    var repos: [Repo] = [Repo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationBarTitle = repos[indexPath.row].title
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(RepoCell.self)", for: indexPath) as! RepoCell
+        let repo = repos[indexPath.row]
+        
+        cell.titleLabel.text = repo.title
+        cell.descriptionLabel.text = repo.description
+        cell.url = URL(string: repo.url)
+        
+        return cell
+    }
+}
