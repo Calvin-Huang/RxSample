61ac49ce90d30f0ebd656a50f0d831eb36461c3a
Calvin <calvin.peak@capslock.tw>
2016-10-13 17:42:59 +0900

    Complete api loading.

--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+// import Moya_ObjectMapper
+import ObjectMapper
+
 class ViewController: UIViewController {
     @IBOutlet var navigationBar: UINavigationBar!
     @IBOutlet var tableView: UITableView!
@@ -28,6 +31,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
 
@@ -41,6 +47,10 @@ extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         navigationBarTitle = repos[indexPath.row].title
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 74
+    }
 }
 
 extension ViewController: UITableViewDataSource {
@@ -52,10 +62,62 @@ extension ViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "\(RepoCell.self)", for: indexPath) as! RepoCell
         let repo = repos[indexPath.row]
         
+        cell.viewController = self
         cell.titleLabel.text = repo.title
         cell.descriptionLabel.text = repo.description
         cell.url = URL(string: repo.url)
         
         return cell
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        GitHubProvider.request(.searchRepo(searchText)) { [weak self] (result) in
+            
+            var errorMessage = ""
+            var success = true
+            
+            switch result {
+            case let .success(response):
+                if
+                    let json = try? JSONSerialization.jsonObject(with: response.data) as? [String: Any],
+                    let items = json?["items"] as? [[String: Any]],
+                    let repos = Mapper<Repo>().mapArray(JSONArray: items)
+                {
+                    
+                    self?.repos = repos
+                } else {
+                    self?.repos = []
+                }
+                self?.tableView.reloadData()
+            case let .failure(error):
+                guard let error = error as? CustomStringConvertible else {
+                    break
+                }
+                
+                errorMessage = error.description
+                success = false
+            }
+            
+            if !success {
+                let alertController = UIAlertController(title: "Error!!", message: errorMessage, preferredStyle: .alert)
+                alertController.addAction(
+                    UIAlertAction(title: "OK", style: .default, handler: { _ in
+                        self?.dismiss(animated: true, completion: nil)
+                    })
+                )
+                
+                self?.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if searchBar.isFirstResponder {
+            searchBar.resignFirstResponder()
+        }
     }
 }
