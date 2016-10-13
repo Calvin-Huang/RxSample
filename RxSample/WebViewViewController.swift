07ecf072229df24dbf74e04eeb871ba79da08d4a
Calvin <calvin.peak@capslock.tw>
2016-10-13 14:15:48 +0900

    Add webviewController.

---
+++ RxSample/WebViewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewViewController.swift
+//  RxSample
+//
+//  Created by Calvin on 10/13/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import UIKit
+
+class WebViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
