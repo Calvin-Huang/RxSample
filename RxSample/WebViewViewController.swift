4c333aa7f46c91c532a168e14793d88c582e8df7
Calvin <calvin.peak@capslock.tw>
2016-10-15 05:16:13 +0900

    Convert to Rx style.

--- RxSample/WebViewViewController.swift
+++ RxSample/WebViewViewController.swift
@@ -8,41 +8,43 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class WebViewViewController: UIViewController {
     @IBOutlet var webView: UIWebView!
     @IBOutlet var undoButton: UIBarButtonItem!
     @IBOutlet var redoButton: UIBarButtonItem!
+    @IBOutlet var dismissButton: UIBarButtonItem!
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        webView.delegate = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        webView.rx.observe(Bool.self, "canGoBack")
+            .asDriver(onErrorJustReturn: false)
+            .map { $0 ?? false }
+            .drive(undoButton.rx.enabled)
+            .addDisposableTo(disposeBag)
+        
+        webView.rx.observe(Bool.self, "canGoForward")
+            .asDriver(onErrorJustReturn: false)
+            .map { $0 ?? false }
+            .drive(redoButton.rx.enabled)
+            .addDisposableTo(disposeBag)
+        
+        undoButton.rx.tap
+            .subscribe { _ in self.webView.goBack() }
+            .addDisposableTo(disposeBag)
+        
+        redoButton.rx.tap
+            .subscribe { _ in self.webView.goForward() }
+            .addDisposableTo(disposeBag)
+        
+        dismissButton.rx.tap
+            .subscribe { _ in self.dismiss(animated: true, completion: nil) }
+            .addDisposableTo(disposeBag)
     }
     
-
-    @IBAction func dismissButtonClicked(_: AnyObject) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func undoButtonClicked(_: AnyObject) {
-        webView.goBack()
-    }
-    
-    @IBAction func redoButtonClicked(_: AnyObject) {
-        webView.goForward()
-    }
-
-}
-
-extension WebViewViewController: UIWebViewDelegate {
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        undoButton.isEnabled = webView.canGoBack
-        redoButton.isEnabled = webView.canGoForward
-    }
 }
